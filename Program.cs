08408d0f3c1df5547daca9c775dd7d0d77571588
JrGustavo <gus@soypetal.com>
2024-11-18 16:10:02 -0500

    Update course C

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+var sale = new SaleWithTax(15);
+var message = sale.GetInfo();
+
+
+Console.WriteLine(message);
+
+class SaleWithTax : Sale
+{
+    public SaleWithTax(decimal total) : base(total)
+    {
+    }
+    
+}
+
+class Sale
+{
+    public decimal Total { get; set;  }
+    
+    public Sale(decimal total)
+    {
+        Total = total;
+    }   
+    
+    public string GetInfo()
+    {
+        return "El Total es " + Total; 
+    }
+}
+
